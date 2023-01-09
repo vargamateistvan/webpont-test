94b8a769d34f7d2e47fd9c64c9d643b694577f4d
MateIstvanVarga <mateistvanvarga@gmail.com>
2023-01-09 12:02:51 +0100

    Bootstrap fix

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import "mutationobserver-shim";
 import { createApp } from "vue";
 import App from "./App.vue";
 import i18n from "./i18n";
+import "@popperjs/core";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+require("bootstrap");
+require("popper.js");
 
 createApp(App).use(i18n).mount("#app");
