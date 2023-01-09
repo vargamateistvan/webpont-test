07cc0a6ce71562dfb73767037209ac53b3566e53
MateIstvanVarga <mateistvanvarga@gmail.com>
2023-01-09 11:42:07 +0100

    Init vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18n from './i18n'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
