4716f1a4daebf73cb553a18e224a11692249c5b9
MateIstvanVarga <mateistvanvarga@gmail.com>
2023-01-09 11:49:45 +0100

    Remove bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-import i18n from './i18n'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import { createApp } from "vue";
+import App from "./App.vue";
+import i18n from "./i18n";
 
-createApp(App).use(i18n).mount('#app')
+createApp(App).use(i18n).mount("#app");
